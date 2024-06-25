0ef9b088d7fa906b45d724fb6c0e9e1ad932d97c
ndtai1527 <ductai1527@gmail.com>
2024-06-26 00:44:01 +0700

    fix something

--- vanilla.sh
+++ vanilla.sh
@@ -140,7 +140,7 @@ for file in "${files_to_copy[@]}"; do
     fi
 done
 
-util_folder=$(find "$FRAMEWORK_DIR" -type d -path "*/internal/util" | head -n 1)
+util_folder=$(find "$FRAMEWORK_DIR" -type d -path "*/classes5.dex.out/com/android/internal/util" | head -n 1)
 
 if [[ -d "$util_folder" ]]; then
     summert_folder="$util_folder/summert"
